331b4e131ad3397ccc3794dafd6f657544bf7ede
Patryk <patrykyrtap@o2.pl>
2020-02-04 20:22:56 +0100

    Add animation
    Button "get started" appear from bottom

--- js/scroll.js
+++ js/scroll.js
@@ -2,16 +2,6 @@ const btnToStart = document.querySelector('.landing__button');
 const btnScrollToTop = document.querySelector('#btnScrollToTop');
 const elm = document.querySelector('#start');
 
-/*btnScrollToTop.addEventListener("click", ()=>{
-    //window.scrollTo(0, 0);
-    
-    window.scrollTo({
-       top: 60,
-       left: 0,
-       behavior: "smooth" 
-    });
-
-});*/
 /* SCROLL UP / SCROLL DOWN */
 
 btnToStart.addEventListener("click", () => scrollIt(elm));
@@ -43,12 +33,3 @@ window.addEventListener('scroll', () => {
         displayArrow.style.display = "none";
     }
 });
-
-
-/*if (window.getComputedStyle(btnScrollToTop).display === "none" ){
-    document.getElementById("btnScrollToTop").style.display = "block";
-}*/
-/*const rect = btnToStart.getBoundingClientRect();
-console.log(rect.x);
-console.log(DOMRect.height);
-console.log(window.scrollY);*/
